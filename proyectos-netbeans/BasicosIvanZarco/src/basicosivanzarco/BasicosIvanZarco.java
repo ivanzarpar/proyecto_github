c70b55916e92f04638c39e779504d65a34326817
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-11-13 17:23:32 +0100

    Trabajando en la rama cadenas

--- proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
+++ proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
@@ -1,21 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basicosivanzarco;
-
-/**
- *
+/*
  * @author ivan_
  */
 public class BasicosIvanZarco {
+public static void cadenas() {
 
-    /**
-     * @param args the command line arguments
-     */
+        String palabrita;
+        
+        
+        palabrita = "hola";
+        
+        
+        
+        System.out.print("Mi palabrita es: ");
+        System.out.println(palabrita);
+
+        
+       
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+       cadenas();  
     }
     
 }
