6904e9f570932ae2c61215114579dfaee776229b
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-11-13 18:36:11 +0100

    Trabajando en la rama numericos

--- proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
+++ proyectos-netbeans/BasicosIvanZarco/src/basicosivanzarco/BasicosIvanZarco.java
@@ -3,23 +3,31 @@ package basicosivanzarco;
  * @author ivan_
  */
 public class BasicosIvanZarco {
-public static void cadenas() {
+/**
+     * Método para mostrar ejemplos de tipos numericos.
+     */
+    public static void numericos() {
 
-        String palabrita;
-        
-        
-        palabrita = "hola";
-        
-        
-        
-        System.out.print("Mi palabrita es: ");
-        System.out.println(palabrita);
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
 
-        
-       
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
     }
     public static void main(String[] args) {
-       cadenas();  
+       numericos();  
     }
     
 }
